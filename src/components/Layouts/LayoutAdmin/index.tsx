a83b3e3a3549bb1b33b447fba62d73a16f84eb8d
Nguyen Hong Thai <thainhph37455@fpt.edu.vn>
2024-09-18 15:34:12 +0700

    Admin Dashboard

---
+++ src/components/Layouts/LayoutAdmin/index.tsx
@@ -0,0 +1,82 @@
+import {
+    BankOutlined,
+    BellOutlined,
+    CreditCardOutlined,
+    HomeOutlined,
+    SettingOutlined,
+    SwapOutlined,
+    UserOutlined,
+    WalletOutlined,
+} from '@ant-design/icons';
+import { Avatar, Badge, Input, Layout, Space } from 'antd';
+import { Outlet } from 'react-router-dom';
+// Import component AdminDashboard
+
+const { Header, Sider, Content } = Layout;
+const LayoutAdmin = () => {
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            {/* Sidebar */}
+            <Sider width={230} style={{ backgroundColor: '#f0f2f5', paddingLeft: '20px' }}>
+                <div className="text-blue-600 font-bold mb-10 flex items-center space-x-2 p-5 text-28px">
+                    <BankOutlined style={{ fontSize: '24px', color: '#1890ff' }} />
+                    <span>BankDash.</span>
+                </div>
+                <nav>
+                    <ul className="space-y-7 px-5 color-gray font-semibold">
+                        <li className="flex items-center">
+                            <HomeOutlined className="mr-6" /> Dashboard
+                        </li>
+                        <li className="flex items-center">
+                            <SwapOutlined className="mr-6" /> Transactions
+                        </li>
+                        <li className="flex items-center">
+                            <WalletOutlined className="mr-6" /> Accounts
+                        </li>
+                        <li className="flex items-center">
+                            <BankOutlined className="mr-6" /> Investments
+                        </li>
+                        <li className="flex items-center">
+                            <CreditCardOutlined className="mr-6" /> Credit Cards
+                        </li>
+                        <li className="flex items-center">
+                            <SettingOutlined className="mr-6" /> Services
+                        </li>
+                        <li className="flex items-center">
+                            <SettingOutlined className="mr-6" /> Settings
+                        </li>
+                    </ul>
+                </nav>
+            </Sider>
+
+            {/* Main Content */}
+            <Layout>
+                {/* Header */}
+                <Header className="bg-white shadow p-5 flex justify-between items-center">
+                    <h3 className="ml-5 text-28px font-semibold">Overview</h3>
+                    <div className="flex items-center space-x-4">
+                        <Input.Search placeholder="Search for something" style={{ width: 400 }} />
+                    </div>
+                    <div className="flex items-center space-x-4 mt-3">
+                        <Space size="large">
+                            <button>
+                                <SettingOutlined style={{ fontSize: '24px', color: '#8c8c8c' }} />
+                            </button>
+                            <button>
+                                <Badge className="mr-10" count={9}>
+                                    <BellOutlined style={{ fontSize: '24px', color: '#ff4d4f' }} />
+                                </Badge>
+                            </button>
+                        </Space>
+                        <button className="pb-6">
+                            <Avatar style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} />
+                        </button>
+                    </div>
+                </Header>
+                <Outlet />
+            </Layout>
+        </Layout>
+    );
+};
+
+export default LayoutAdmin;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,9 @@
 import { useRoutes } from 'react-router-dom';
 import routerClient from './routerClient';
+import routerAdmin from './routerAdmin';
 
 const Routes = () => {
-    const routes = useRoutes([routerClient]);
+    const routes = useRoutes([routerClient, ...routerAdmin]); // Gộp hai router lại thành một mảng
     return routes;
 };
 
