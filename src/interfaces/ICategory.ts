f86738087184d5871a54a8c8a45bb420733616a0
Nguyen Hong Thai <thainhph37455@fpt.edu.vn>
2024-10-23 20:37:11 +0700

    update-admin-category

--- src/interfaces/ICategory.ts
+++ src/interfaces/ICategory.ts
@@ -1,7 +1,10 @@
+import { IProduct } from './IProduct';
+
 export interface ICategory {
     id: string | number;
     name: string;
     slug: string | number;
+    products?: IProduct[];
     children?: ICategory[];
     parents?: ICategory[];
 }
