b43d304e8155185666a4fc2571ea29f4ad34a1c3
linh <linhnvph36809@fpt.edu.vn>
2025-01-08 20:54:21 +0700

    fix variant

--- src/pages/Client/Details/index.tsx
+++ src/pages/Client/Details/index.tsx
@@ -37,7 +37,6 @@ const Detail = () => {
 
     const { data, isFetching } = useQueryConfig([QUERY_KEY, `product-detail-${id}`], `/api/product/detail/${id}`);
 
-    console.log(data);
 
     const products = data?.data;
     const { user } = useContextGlobal();
